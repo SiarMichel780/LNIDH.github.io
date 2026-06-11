48bbbe5b741340fbe1f9d1529d290b29509fa190
SiarMichel780 <98641278+SiarMichel780@users.noreply.github.com>
2026-06-11 00:47:45 -0700

    Create firebase-config.js

---
+++ kn7x-v4p2-zm8/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkqPf9ixvTvU5zk9DxdlhQXfqKdcqT3n4",
+  authDomain: "lnidh-plots.firebaseapp.com",
+  projectId: "lnidh-plots",
+  storageBucket: "lnidh-plots.firebasestorage.app",
+  messagingSenderId: "423595215521",
+  appId: "1:423595215521:web:b2f34ae2c6840920f97ee0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
