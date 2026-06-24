50928133618240880f5b57e4397a4ffac0e3315a
SiarMichel780 <98641278+SiarMichel780@users.noreply.github.com>
2026-06-24 15:33:22 -0700

    Update app.js

--- kn7x-v4p2-zm8/app.js
+++ kn7x-v4p2-zm8/app.js
@@ -1,12 +1,12 @@
-alert(“app.js loaded”);
+alert("app.js loaded");
 
-const usernameInput = document.getElementById(“usernameInput”);
-const saveUserBtn = document.getElementById(“saveUserBtn”);
-const loginScreen = document.getElementById(“loginScreen”);
-const mainScreen = document.getElementById(“mainScreen”);
-const welcomeText = document.getElementById(“welcomeText”);
+const usernameInput = document.getElementById(“usernameInput");
+const saveUserBtn = document.getElementById(“saveUserBtn");
+const loginScreen = document.getElementById(“loginScreen");
+const mainScreen = document.getElementById("mainScreen");
+const welcomeText = document.getElementById("welcomeText");
 
-saveUserBtn.addEventListener(“click”, () => {
+saveUserBtn.addEventListener(“click", () => {
 
 alert("button clicked");
 const username = usernameInput.value.trim();
