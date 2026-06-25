de414ebf4031fdfadf95977c526b2428ad7644e9
SiarMichel780 <98641278+SiarMichel780@users.noreply.github.com>
2026-06-24 17:23:25 -0700

    Update app.js

--- kn7x-v4p2-zm8/app.js
+++ kn7x-v4p2-zm8/app.js
@@ -1,27 +1,22 @@
+alert(“JavaScript loaded!”);
+
 const usernameInput = document.getElementById(“username”);
 const loginBtn = document.getElementById(“loginBtn”);
 const login = document.getElementById(“login”);
 const home = document.getElementById(“home”);
 const welcome = document.getElementById(“welcome”);
 
-const savedUsername = localStorage.getItem(“plotsUsername”);
-
-if (savedUsername) {
-login.style.display = “none”;
-home.style.display = “block”;
-welcome.textContent = Welcome ${savedUsername}!;
-}
+console.log(usernameInput);
+console.log(loginBtn);
+console.log(login);
+console.log(home);
+console.log(welcome);
 
 loginBtn.addEventListener(“click”, () => {
 
-const username = usernameInput.value.trim();
-if (username === "") {
-    alert("Please enter a username.");
-    return;
-}
-localStorage.setItem("plotsUsername", username);
+alert("Continue button clicked!");
 login.style.display = "none";
 home.style.display = "block";
-welcome.textContent = `Welcome ${username}!`;
+welcome.textContent = "Welcome!";
 
 });
